e0ece4060e70b68507d10f08b7fb41149d4e3add
jitendraB <jitendrabachhav0435@gmail.com>
2019-11-21 11:41:46 +0530

    AddCode for mobile number validation

--- userreg.sh
+++ userreg.sh
@@ -1,28 +1,57 @@
 	echo"Welcome user Regestration"
 	#!/bin/bash -x
 
-	echo "enter firstName"
-		read  firstName
-	echo "enter lastName"
-		read lastName
-	echo "enter email "
-		read email
-
 	firstNamePattern="^[A-Z][a-zA-Z]{3,}$"
 	lastNamePattern="^[A-Z][a-zA-Z]{3,}$"
 	emailPattern="^[a-zA-Z]{1,}[0-9]*([._+-]{1,}[a-z]+){0,1}[@][a-zA-Z0-9]+[.][a-z]{2,4}([.][a-z]{2}){0,1}"
+	mobilePattern="^[0-9]{2}[ ]{1}[0-9]{10}$"
 
-	if [[ $firstName =~ $firstNamePattern ]] && [[ $lastName =~ $lastNamePattern ]]
-        then
+	function fullNameValidation(){
+		
+		echo "enter firstName"
+		read firstName
+
+		echo "enter lastname"
+		read lastName
+
+		if [[ $firstName =~ $firstNamePattern ]] && [[ $lastName =~ $lastNamePattern ]]
+        	then
                 echo "yes"
-        else
+        	else
                 echo "No"
- 	fi
+ 		fi
+	}
 
-	if [[ $email =~ $emailPattern ]]
-		then
+
+	function emailIdValidation(){
+	
+		echo "enter valid EmailId"
+		read email
+
+		if [[ $email =~ $emailPattern ]]
+			then
 				echo "yes "
 		else
 				echo "enter valid email"
-	fi
+		fi
+	}
 
+
+	function mobileNumberValidation(){
+
+		echo "enetr mobile Number"
+		read mobileNumber
+
+		if [[ $mobileNumber =~ $mobilePattern ]]
+			then
+				echo "yes"
+		else
+				echo "enter valid mobile number"
+		fi
+
+	}
+
+	
+	fullNameValidation
+	emailIdValidation
+	mobileNumberValidation
