1d41aba07770a590f326d4fff230dab51381021d
jitendraB <jitendrabachhav0435@gmail.com>
2019-11-21 11:08:07 +0530

    AddCode for emailValidation

--- userreg.sh
+++ userreg.sh
@@ -1,12 +1,16 @@
-	cho"Welcome user Regestration"
+	echo"Welcome user Regestration"
 	#!/bin/bash -x
+
 	echo "enter firstName"
-	read  firstName
+		read  firstName
 	echo "enter lastName"
-	read lastName
+		read lastName
+	echo "enter email "
+		read email
 
 	firstNamePattern="^[A-Z][a-zA-Z]{3,}$"
 	lastNamePattern="^[A-Z][a-zA-Z]{3,}$"
+	emailPattern="^[a-zA-Z]{1,}[0-9]*([._+-]{1,}[a-z]+){0,1}[@][a-zA-Z0-9]+[.][a-z]{2,4}([.][a-z]{2}){0,1}"
 
 	if [[ $firstName =~ $firstNamePattern ]] && [[ $lastName =~ $lastNamePattern ]]
         then
@@ -15,4 +19,10 @@
                 echo "No"
  	fi
 
+	if [[ $email =~ $emailPattern ]]
+		then
+				echo "yes "
+		else
+				echo "enter valid email"
+	fi
 
