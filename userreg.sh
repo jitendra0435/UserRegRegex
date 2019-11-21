6309342bb37ec3990390f79783f31dd004e33c91
jitendraB <jitendrabachhav0435@gmail.com>
2019-11-21 10:38:07 +0530

    AddCode for valid firstName

--- userreg.sh
+++ userreg.sh
@@ -1,1 +1,16 @@
-echo"Welcome user Regestration"
+	cho"Welcome user Regestration"
+	#!/bin/bash -x
+	echo "enter firstName"
+
+	read  firstName
+
+	firstName="^[A-Z][a-zA-Z]{3,}$"
+
+	if [[ $firstName =~ $firstName ]]
+        then
+                echo "yes"
+        else
+                echo "No"
+ 	fi
+
+
